dd3cbd0df2b56f4128dabddd1349ca8c542dd3be
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-07-16 12:01:34 -0600

    Guia telefonica paso4 Filtrar lista

--- part2/exercises_2.6-2.10/src/components/App.jsx
+++ part2/exercises_2.6-2.10/src/components/App.jsx
@@ -3,26 +3,28 @@ import './App.css'
 
 function App() {
   const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567',
-    },
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ])
   //console.log(persons.map(person => person.name));
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState(persons)
 
   const handleAddPhonebook = (event) => {
     event.preventDefault()
     const names = persons.map(person => person.name)
     const found = names.find(name => name === newName)
-    console.log('Found: ', newNumber);
+    //console.log('Found: ', newNumber);
     if (found === undefined) {
       const personObject = {
         name: newName,
         number: newNumber,
       }
       setPersons(persons.concat(personObject))
+      setNewSearch(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
     } else {
@@ -40,10 +42,24 @@ function App() {
     setNewNumber(event.target.value)
     //console.log(event.target.value);
   }
+  const handleSearch = (event) => {
+    //setNewSearch(event.target.value)
+    //console.log('Search: ', event.target.value);
+    const search = event.target.value.toLowerCase();
+    const filtered = persons.filter(person => 
+      person.name.toLowerCase().includes(search)  
+    )
+    setNewSearch(filtered)
+    //console.log('Search: ', filtered);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          Filter shown with: <input type="text" onChange={handleSearch} />
+      </div>
+      <h2>Add a new</h2>
       <form>
         <div className="inputs">
           <div>
@@ -56,17 +72,17 @@ function App() {
         <div className='button'>
           <button type='submit' onClick={handleAddPhonebook}>add</button>
         </div>
-        <h2>Numbers</h2>
+      </form>
+      <h2>Numbers</h2>
         <div>
           <ul>
             {
-              persons.map(person =>
+              newSearch.map(person =>
                 <li key={person.name}>{person.name} {person.number}</li>  
               )
             }
           </ul>
         </div>
-      </form>
     </div>
   )
 }
