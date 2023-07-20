1dfe3fecdac0c0e157a6f8c94e27bb2a9fd8d229
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-07-19 19:45:57 -0600

    Datos para paises, paso 2 Agregando el boton show a cada pais

--- part2/datosPaises/src/components/App.jsx
+++ part2/datosPaises/src/components/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 function App() {
   const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
   const [find, setFind] = useState([])
 
   useEffect(() => {
@@ -22,21 +23,52 @@ function App() {
     setFind(filtered);
   }
 
+  const handleShowCountry = (country) => {
+    setSelectedCountry(country)
+  }
+
+  const showCountryData = () => {
+    if (selectedCountry) {
+      const { name, capital, population, languages, flags } = selectedCountry
+      return (
+        <div>
+          <h2>{name.common}</h2>
+          <p>Capital: {capital[0]}</p>
+          <p>Population: {population.population}</p>
+          <h2>Languages</h2>
+          <ul>
+            {
+              Object.values(languages).map(language => 
+                <li key={language}>{language}</li>
+              )
+            }
+          </ul>
+          <img src={flags.png} alt={name.common} />
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
   //console.log("Flag: ", Object.values(find[0].languages))
 
   const countriesToShow = () =>{
     if (find.length > 10) {
       return 'Too many matches, specify another filter';
     } else if (find.length > 1) {
-      return find.map(data => (
+      return (
         <ul>
-          <li key={data.name.common}>{data.name.common}</li>
+          {find.map(data => (
+            <li key={data.name.common}>
+              {data.name.common} <button onClick={() => handleShowCountry(data)}>show</button> 
+            </li>
+          ))}
+          {showCountryData()}
         </ul>
-      ));
+      );
     } else if(find.length === 1){
       const country = find[0]
-      //const capital = country.map(data => (data.capital))
-      //const capi = capital.map(data => (data[0]))
       const languages = Object.values(country.languages);
       return (
         <div>
