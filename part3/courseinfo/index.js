8b0dd216d1419c0966f4deeb33f245cf2126de6d
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-08-02 18:33:53 -0600

    part3-2 Middleware

--- part3/courseinfo/index.js
+++ part3/courseinfo/index.js
@@ -3,6 +3,20 @@ const app = express()
 
 app.use(express.json())
 
+//Middleware
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:', request.path);
+    console.log('body:', request.body);
+    console.log('---');
+    next()
+}
+app.use(requestLogger)
+
+const unknownEndpont = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 let notes = [
     {
       "id": 1,
@@ -81,6 +95,8 @@ app.post('/api/notes', (request, response) => {
 
     response.json(note)
 })
+
+app.use(unknownEndpont)
 
 const PORT = 3001
 app.listen(PORT, () => {
